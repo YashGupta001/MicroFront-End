4e33ef24d9d8c2ce9ceaab15dadf3fb052b118f5
Yash Gupta <yash.gupta2@walmart.com>
2023-10-05 23:35:47 +0530

    FInal

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
   return (
-    <div>
-      <h1>Yash Gupta From Top</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
   );
 };
